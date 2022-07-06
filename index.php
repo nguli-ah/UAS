b2076f5c2bfcfff7a636bd7930fd720898640c69
chika <61420450+merchikarzk@users.noreply.github.com>
2022-07-06 10:06:02 +0700

    index.php buat dimasukin ke server

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <!-- Bootstrap CSS -->
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
+
+<!-- Bootstrap Icon -->
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css">
+    <title>Index</title>
+</head>
+<body>
+<div class="container mt-5">
+    <h1 class="text-center"> Yuk cek aplikasi CRUD PHP kami!</h1>
+  <div class="container">
+    <form action="./penerbit/index.php" method="post">
+        <div class="from-group text-center">
+            <input type="submit" class="btn btn-primary mt-2" value="Let's Go!">
+        </div>
+    </form>
+  </div>
+</div>
+</body>
+</html>
